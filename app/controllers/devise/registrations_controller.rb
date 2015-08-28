90331d52d913ce1eef7e0ec5b40f9c42cb9c1284
Emiliano Ritiro <eritiro@gmail.com>
2015-08-28 00:05:48 -0300

    Devise for rails 5

--- app/controllers/devise/registrations_controller.rb
+++ app/controllers/devise/registrations_controller.rb
@@ -20,11 +20,11 @@ class Devise::RegistrationsController < DeviseController
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_flashing_format?
         sign_up(resource_name, resource)
-        respond_with resource, location: after_sign_up_path_for(resource)
+        redirect_to after_sign_up_path_for(resource)
       else
         set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
         expire_data_after_sign_in!
-        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+        redirect_to after_inactive_sign_up_path_for(resource)
       end
     else
       clean_up_passwords resource
@@ -54,7 +54,7 @@ class Devise::RegistrationsController < DeviseController
         set_flash_message :notice, flash_key
       end
       sign_in resource_name, resource, bypass: true
-      respond_with resource, location: after_update_path_for(resource)
+      redirect_to after_update_path_for(resource)
     else
       clean_up_passwords resource
       respond_with resource
